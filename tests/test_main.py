19182c3e7aba7866af0960b999d71c4cc4eb931c
Bradley Hu <hu.yu@wcs-g.com>
2025-09-04 22:51:02 +0000

    test stuff

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,7 +8,9 @@ def test_1():
 def test_2():
     print("Testing replaceatpos(\"aaaaa\",2,\"b\")!")
     print(BradleysStrings.replaceatpos("aaaaa",2,"b"))
-
+def test_3():
+    print("Testing replaceinstr(...)!")
+    print(BradleysStrings.replaceinstr("aaaaabbbbbccccc123",replacedarray = ["b","1"],replacewitharr = ["r","4"]))
 
 test_1()
 test_2()
