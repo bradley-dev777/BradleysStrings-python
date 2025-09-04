a079a3d8a9fce36f056e0d648ff7bdb6f8683351
Bradley Hu <hu.yu@wcs-g.com>
2025-09-04 00:17:11 +0000

    first commit

--- src/BradleysStrings/BradleysStrings.py
+++ src/BradleysStrings/BradleysStrings.py
@@ -1,1 +1,6 @@
-
+def insert(mainstr,pos,insertstr):
+    mainstr1 = mainstr[:pos]
+    mainstr2 = mainstr[pos:]
+    return mainstr1 + insertstr + mainstr2
+def splitatpos(str,pos):
+    return [str[:pos],str[pos:]]
---
+++ tests/test_main.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.insert(1,"/workspaces/BradleysStrings-python/src/BradleysStrings/")
+import BradleysStrings
+def test_1():
+    print("Testing function insert()!")
+    print(BradleysStrings.insert("aaaaa",2,"b"))
